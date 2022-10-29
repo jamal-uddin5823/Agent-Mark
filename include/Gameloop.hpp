59875b4c95708eca5d36661055f1f218c8804ef6
Jamal Uddin Mallick <103067313+jamal-uddin5823@users.noreply.github.com>
2022-10-29 23:46:46 +0600

    Update Gameloop.hpp

--- include/Gameloop.hpp
+++ include/Gameloop.hpp
@@ -13,11 +13,12 @@ const int VEL_X=20;
 double ENEMY_VEL=0.25;
 
 
-int agent_frame = 0,enemy_frame=0;
+int agent_frame_no = 0,enemy_frame_no=0;
 
-Entity curr_agent_frame = running_agent[agent_frame/running_agent.size()];
-Entity curr_enemy_frame = running_enemy[enemy_frame/running_enemy.size()];
+Entity curr_agent_frame = running_agent[agent_frame_no/running_agent.size()];
+Entity curr_enemy_frame = running_enemy[enemy_frame_no/running_enemy.size()];
 std::pair<int,int> movement;
+int flag=0;
 
 void Handle_event(SDL_Event& e, bool& gameRunning){
     while (SDL_PollEvent(&e))
@@ -25,7 +26,7 @@ void Handle_event(SDL_Event& e, bool& gameRunning){
         if(e.type == SDL_QUIT){
             gameRunning=false;
         }
-        movement = curr_agent_frame.handleEvent(e);
+        movement = curr_agent_frame.handleEvent(e,&flag);
     }
 }
 
@@ -36,8 +37,6 @@ void gameloop(){
         window.changeRenderColor(255,255,255,255);
         // window.render(background,1);
 
-
-        //bg scroll
         static int scrollingOffset = 0;
         scrollingOffset-=5;
 
@@ -50,43 +49,82 @@ void gameloop(){
 
 
 
-        int blackBox_x=0,blackBox_y = SCREEN_HEIGHT/4-32;
-        
-        curr_agent_frame = running_agent[agent_frame/running_agent.size()];
-        curr_enemy_frame = running_enemy[enemy_frame/running_enemy.size()];
 
+        if(flag==0)
+            curr_agent_frame = running_agent[agent_frame_no/running_agent.size()];
+        else if(flag==1)
+            curr_agent_frame = jumping_agent[agent_frame_no/jumping_agent.size()];
+        else
+            curr_agent_frame = sliding_agent[agent_frame_no/sliding_agent.size()];
+            
+        curr_enemy_frame = running_enemy[enemy_frame_no/running_enemy.size()];
         
-        window.render(curr_agent_frame,1);
+
+
+
+        if(flag==0|| flag==-1)
+            window.render(curr_agent_frame,1);
+        else if(flag==1)
+            window.renderjump(curr_agent_frame,3);
         window.render(curr_enemy_frame,1);
-        running_enemy[enemy_frame/running_enemy.size()].changepos(0.25,0);
+        // running_enemy[enemy_frame_no/running_enemy.size()].changepos(ENEMY_VEL,0);
 
 
 
-        agent_frame++;
-        enemy_frame++;
+        agent_frame_no++;
+        enemy_frame_no++;
         for (int i = 0; i < (int)running_agent.size(); i++)
         {
             running_agent[i].move(movement.first,movement.second);
         }
+        for (int i = 0; i < (int)jumping_agent.size(); i++)
+        {
+            jumping_agent[i].move(movement.first,movement.second);
+        }
+        for (int i = 0; i < (int)sliding_agent.size(); i++)
+        {
+            sliding_agent[i].move(movement.first,movement.second);
+        }
+
+        // for (int i = 0; i < (int)running_enemy.size(); i++)
+        // {
+        //     running_enemy[i].move(ENEMY_VEL,0);
+        // }
+
+
+
+
+
+
         
+        int blackBox_x=0,blackBox_y = SCREEN_HEIGHT/3-32;
         for (int i = 0; i < 40; i++)
         {
             Entity newgrass = Entity(Vector2f(blackBox_x,blackBox_y),blackBoxTexture,32,32,0,0);
             // int randNum = rand()%(100-1 + 1) +1;
             // if(randNum!=2 || randNum!=32)
-            window.render(newgrass,4);
+            window.render(newgrass,3);
             blackBox_x+=32;
-            // SDL_Delay(1000/900);
         }
 
-        if(agent_frame/running_agent.size()>=running_agent.size()){
-            agent_frame=0;
+
+
+        if(agent_frame_no/running_agent.size()>=running_agent.size() && flag==0){
+            agent_frame_no=0;
         }
-        if(enemy_frame/running_enemy.size()>=running_enemy.size()){
-            enemy_frame=0;
-            ENEMY_VEL+=0.2;
+        if(agent_frame_no/jumping_agent.size()>=jumping_agent.size() && flag==1){
+            agent_frame_no=0;
+            flag=0;
         }
-        
+        if(agent_frame_no/sliding_agent.size()>=sliding_agent.size() && flag==-1){
+            agent_frame_no=0;
+            flag=0;
+        }
+        if(enemy_frame_no/running_enemy.size()>=running_enemy.size()){
+            enemy_frame_no=0;
+            // ENEMY_VEL+=0.1;
+        }
+    
         window.display();
         SDL_Delay(1000/30);
 
