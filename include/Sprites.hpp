81f768422ad440126e5dcb72837e3be726ba5249
Jamal Uddin Mallick <103067313+jamal-uddin5823@users.noreply.github.com>
2022-10-29 23:48:01 +0600

    Update Sprites.hpp

--- include/Sprites.hpp
+++ include/Sprites.hpp
@@ -16,29 +16,49 @@ int blackBox_x=0,blackBox_y = SCREEN_HEIGHT/3-32;
 SDL_Texture* blackBoxTexture = window.loadTexture("res/Blackbox.png",0,0,0,0); 
 SDL_Texture* backTexture = window.loadTexture("res/skyline.png",1,255,255,255);
 SDL_Texture* agentTexture = window.loadTexture("res/sheet.png",1,255,255,255);
-SDL_Texture* enemyTexture = window.loadTexture("res/enemysheet.png",0,255,0,0); 
+SDL_Texture* enemyTexture = window.loadTexture("res/enemysheet.png",0,255,0,0);
+SDL_Texture* colliderTexture = window.loadTexture("res/Blackbox.png",0,0,0,0); 
 
 
 
 
 Entity standing_boy = Entity(Vector2f(100,SCREEN_HEIGHT-210),agentTexture,125,125,0,0);
+// Entity newgrass = Entity(Vector2f(blackBox_x,blackBox_y),blackBoxTexture,32,32,0,0);
+Entity newgrass = Entity(Vector2f(blackBox_x,blackBox_y),blackBoxTexture,32,32,0,0);
+
+
 
 std::vector<Entity> running_agent ={
-    // Entity(Vector2f(0,SCREEN_HEIGHT-200),agentTexture,125,125,0,0),
-    // Entity(Vector2f(0,SCREEN_HEIGHT-200),agentTexture,125,125,250,125),
-    Entity(Vector2f(200,SCREEN_HEIGHT-240),agentTexture,129,129,387,129),
-    Entity(Vector2f(205,SCREEN_HEIGHT-240),agentTexture,129,129,516,129),
-    Entity(Vector2f(210,SCREEN_HEIGHT-240),agentTexture,129,129,645,129),
-    Entity(Vector2f(215,SCREEN_HEIGHT-240),agentTexture,129,129,774,129)
+    Entity(Vector2f(200,SCREEN_HEIGHT-210),agentTexture,129,129,387,129),
+    Entity(Vector2f(205,SCREEN_HEIGHT-210),agentTexture,129,129,516,129),
+    Entity(Vector2f(210,SCREEN_HEIGHT-210),agentTexture,129,129,645,129),
+    Entity(Vector2f(215,SCREEN_HEIGHT-210),agentTexture,129,129,774,129)
+};
+
+
+std::vector<Entity> jumping_agent ={
+    Entity(Vector2f(200,SCREEN_HEIGHT-210),agentTexture,129,129,129*2,129*3),
+    Entity(Vector2f(200,SCREEN_HEIGHT-210),agentTexture,129,129,129*3,129*3),
+    Entity(Vector2f(200,SCREEN_HEIGHT-210),agentTexture,129,129,129*4,129*3),
+    Entity(Vector2f(200,SCREEN_HEIGHT-210),agentTexture,129,129,129*5,129*3)
+    
+};
+std::vector<Entity> sliding_agent ={
+    Entity(Vector2f(200,SCREEN_HEIGHT-210),agentTexture,129,129,129*1,129*4),
+    Entity(Vector2f(205,SCREEN_HEIGHT-210),agentTexture,129,129,129*2,129*4),
+    Entity(Vector2f(210,SCREEN_HEIGHT-210),agentTexture,129,129,129*3,129*4),
+    Entity(Vector2f(215,SCREEN_HEIGHT-210),agentTexture,129,129,129*4,129*4),
+    Entity(Vector2f(215,SCREEN_HEIGHT-210),agentTexture,129,129,129*5,129*4),
+    Entity(Vector2f(215,SCREEN_HEIGHT-210),agentTexture,129,129,129*6,129*4)
 };
 
 std::vector<Entity> running_enemy ={
-    // Entity(Vector2f(0,SCREEN_HEIGHT-200),agentTexture,125,125,0,0),
-    // Entity(Vector2f(0,SCREEN_HEIGHT-200),agentTexture,125,125,250,125),
-    Entity(Vector2f(15,SCREEN_HEIGHT-240),enemyTexture,129,129,516,129),
-    Entity(Vector2f(20,SCREEN_HEIGHT-240),enemyTexture,129,129,645,129),
-    Entity(Vector2f(25,SCREEN_HEIGHT-240),enemyTexture,129,129,774,129),
-    Entity(Vector2f(10,SCREEN_HEIGHT-240),enemyTexture,129,129,387,129)
+    Entity(Vector2f(15,SCREEN_HEIGHT-210),enemyTexture,129,129,516,129),
+    Entity(Vector2f(20,SCREEN_HEIGHT-210),enemyTexture,129,129,645,129),
+    Entity(Vector2f(25,SCREEN_HEIGHT-210),enemyTexture,129,129,774,129),
+    Entity(Vector2f(10,SCREEN_HEIGHT-210),enemyTexture,129,129,387,129)
 };
+
+// Entity collider = Entity(Vector2f(64,32),colliderTexture,32,32,0,0);
 
 Entity background = Entity(Vector2f(0,0),backTexture,2560,960,0,0);
