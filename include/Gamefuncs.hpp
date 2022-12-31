72a84b149bfa277e2acd4a95b9669c31e8b60ee2
Jamal Uddin Mallick <103067313+jamal-uddin5823@users.noreply.github.com>
2023-01-01 00:13:00 +0600

    Update Gamefuncs.hpp

--- include/Gamefuncs.hpp
+++ include/Gamefuncs.hpp
@@ -14,6 +14,7 @@
 #include "Music.hpp"
 #include "Fileio.hpp"
 #include "Mouse.hpp"
+#include "Entity.hpp"
 
 #define NEW_GAME false
 #define CONTINUE_PREV_GAME true
@@ -34,16 +35,27 @@ int button_arr[TOTAL_BUTTONS] = {840,107,255,403,556,701};
 
 int first_page_time = 0; //counter to count time spent on startscreen
 
-
+int add=0;
 const int VEL_X=20;
 double ENEMY_VEL=0.25;
 int agent_frame_no = 0,enemy_frame_no=0;
+bool paused_flag=false;
 bool obsflag=0,lifeflag=0;
+bool coinflag=0, freerunflag=0;
 int agent_frame_select_flag=0;
 int initial_score = 0;
 bool first_loop = false;
 bool life_present_prev = false;
+bool coin_present_prev= false;
+bool freerun_present_prev=false;
+int noDamage=-1;
+int prev_paused=0, prev_not_paused=0;
 std::pair<int,int> movement;
+bool showing=false; 
+bool prev_calced=true;
+int score_temp=0;
+int extra=0;
+int prev_score=0;
 
 extern int time_passed;
 int extra_time = 0;
@@ -65,13 +77,15 @@ void render_ground();
 void reset_frame_no();
 void collision_checker(bool& gameRunning);
 void render_lifeline();
+void render_coin();
+void render_freerun();
 int generate_score();
 
 
 
 void init_score_life(){
     if(continue_flag==CONTINUE_PREV_GAME){
-        read_history(&initial_score,&life,&OBSTACLE_SPEED,&life_present_prev);
+        read_history(&prev_score,&life,&OBSTACLE_SPEED,&life_present_prev);
     }
 }
 
@@ -79,7 +93,7 @@ void init_score_life(){
 
 void background_scroll(){
     static int scrollingOffset = 0;
-    scrollingOffset-=5;
+    if(!paused_flag)scrollingOffset-=5;
 
     if(scrollingOffset< -background.getCurrentFrame().w)
         scrollingOffset = 0;
@@ -118,29 +132,59 @@ void render_enemy(){
 
 void render_obstacle(){
     for(int i=0;i<(int)obstacle_array.size();i++){
-        if(abs(obstacle_array[0].getpos().x-obstacle_array[1].getpos().x-obstacle_array[1].getCurrentFrame().w)<100){
-            obstacle_array[1].changepos(500,0);
-            // obstacle_array[1].setpos(-100,600);
+
+        if((int)SDL_GetTicks()<noDamage)
+        {
+            int current_pos=obstacle_array[i].getpos().x;
+            obstacle_array[i].changepos(current_pos-500,0);
+            continue;
+        }
+
+
+        if(i!=0 && abs((obstacle_array[i].getpos().x-obstacle_array[i-1].getpos().x))<60)
+        {
+            int fir=obstacle_array[i].getpos().x;
+            int sec=obstacle_array[i-1].getpos().x;
+            if(fir>sec)obstacle_array[i].changepos(sec+100, 0);
+            else obstacle_array[i-1].changepos(fir+100, 0);
+        }
+        else if(i==0 && abs((obstacle_array[0].getpos().x-obstacle_array[obstacle_array.size()-1].getpos().x))<60)
+        {
+            int fir=obstacle_array[obstacle_array.size()-1].getpos().x;
+            int sec=obstacle_array[0].getpos().x;
+            if(fir<sec)obstacle_array[i].changepos(fir+60, 0);
+            else obstacle_array[obstacle_array.size()-1].changepos(sec+60, 0);
+        }
+        if(!paused_flag){
+            if(abs(obstacle_array[0].getpos().x-obstacle_array[1].getpos().x-obstacle_array[1].getCurrentFrame().w)<100){
+                obstacle_array[1].changepos(500,0);
+                // obstacle_array[1].setpos(-100,600);
+            }
         }
         window.renderObstacle(obstacle_array[i],obsflag,&OBSTACLE_SPEED);
-        int coin = window.random(1,100);
-        if(coin>50)
-            obsflag=!obsflag;
+            int coin = window.random(1,100);
+            
+            if(coin>50)
+                obsflag=!obsflag;
     }
 }
 
 void update_agent_pos(){
-    for (int i = 0; i < (int)running_agent.size(); i++)
-    {
-        running_agent[i].changepos(movement.first,movement.second);
-    }
-    for (int i = 0; i < (int)jumping_agent.size(); i++)
-    {
-        jumping_agent[i].changepos(movement.first,movement.second);
-    }
-    for (int i = 0; i < (int)sliding_agent.size(); i++)
-    {
-        sliding_agent[i].changepos(movement.first,movement.second);
+
+    if(!paused_flag){
+
+        for (int i = 0; i < (int)running_agent.size(); i++)
+        {
+            running_agent[i].changepos(movement.first,movement.second);
+        }
+        for (int i = 0; i < (int)jumping_agent.size(); i++)
+        {
+            jumping_agent[i].changepos(movement.first,movement.second);
+        }
+        for (int i = 0; i < (int)sliding_agent.size(); i++)
+        {
+            sliding_agent[i].changepos(movement.first,movement.second);
+        }
     }
 }
 
@@ -177,8 +221,11 @@ void collision_checker(bool& gameRunning){
             bool collideObstacle=curr_agent_frame.checkCollision(curr_agent_frame,obstacle_array[i],agent_frame_select_flag);
             bool collideEnemy=curr_agent_frame.checkCollision(curr_agent_frame,curr_enemy_frame,agent_frame_select_flag);
             bool collideLifeline = curr_agent_frame.checkCollision(curr_agent_frame,lifeline,agent_frame_select_flag);
+            bool collideCoin = curr_agent_frame.checkCollision(curr_agent_frame,coin,agent_frame_select_flag);
+            bool collideFreerun = curr_agent_frame.checkCollision(curr_agent_frame,freerun,agent_frame_select_flag);
 
-            if(collideObstacle==true || collideEnemy==true){
+
+            if((collideObstacle==true || collideEnemy==true) && noDamage<(int)(SDL_GetTicks())){
                 Mix_PlayChannel(-1,collision,0);
                 if(collideEnemy==true){
                     life=0;
@@ -187,18 +234,20 @@ void collision_checker(bool& gameRunning){
                     gameRunning=false;
                     return;
                 }
-                if(collideObstacle==true)
+                if(collideObstacle==true){
                     obstacle_array[i].getpos().x=-100;
-
-
-
-                life--;
+                    life--;
+                    //printf("%d ", life);
+                }
                 if(life<=0){
+                    //printf("Hello\n");
                     Mix_PlayChannel(-1,death,0);
                     SDL_Delay(1000);
+                    game_status=GAMEOVER;
                     gameRunning=false;
                 }
                 collideObstacle=false;
+                collideEnemy=false;
 
                 for (int i = 0; i < (int)running_enemy.size(); i++)
                 {
@@ -211,36 +260,113 @@ void collision_checker(bool& gameRunning){
                 life++;
                 lifeline.getpos().x=-100;
             }
+
+            if(collideCoin==true){
+                Mix_PlayChannel(-1,lifeup,0);
+                extra-=10;
+                coin.getpos().x=-100;
+            }
+
+            if(collideFreerun==true){
+                Mix_PlayChannel(-1,lifeup,0);
+                noDamage=(int)(SDL_GetTicks())+5000;
+                freerun.getpos().x=-100;
+            }
         }
 }
 
 
 void render_lifeline(){
-    int lifecoin = window.random(1,1000);
-    if(lifecoin>995){
+    int lifecoin = window.random(1,500);
+    if(lifecoin>495 && !showing){
         lifeflag=1;
+        showing=true;
     }
-    if(lifeflag||life_present_prev){
+    if(lifeflag|| life_present_prev){
         window.render(lifeline);
-        lifeline.changepos(-5,0);
+        if(!paused_flag)lifeline.changepos(-5,0);
         if(lifeline.getpos().x+lifeline.getCurrentFrame().w<0){
             lifeline.setpos(SCREEN_WIDTH,SCREEN_HEIGHT-200);
             lifeflag=false;
+            showing=false;
         }
         life_present_prev=false;
     }
 }
 
+void render_coin(){
+    int coincoin = window.random(1,500);
+    if(coincoin>495 && !showing){
+        coinflag=1;
+        showing=true;
+    }
+    if(coinflag||coin_present_prev){
+        window.render(coin);
+        if(!paused_flag)coin.changepos(-5,0);
+        if(coin.getpos().x+coin.getCurrentFrame().w<0){
+            coin.setpos(SCREEN_WIDTH,SCREEN_HEIGHT-200);
+            coinflag=false;
+            showing=false;
+        }
+        coin_present_prev=false;
+    }
+}
+
+void render_freerun(){
+    int freeruncoin = window.random(1,500);
+    if(freeruncoin>495 && !showing){
+        freerunflag=1;
+        showing=true;
+    }
+    if(freerunflag||freerun_present_prev){
+        window.render(freerun);
+        if(!paused_flag)freerun.changepos(-5,0);
+        if(freerun.getpos().x+freerun.getCurrentFrame().w<0){
+            freerun.setpos(SCREEN_WIDTH,SCREEN_HEIGHT-200);
+            freerunflag=false;
+            showing= false;
+        }
+        freerun_present_prev=false;
+    }
+}
+
+
 
 int generate_score(){
+
+    //printf("%d ",prev_score);
+    if(paused_flag)
+    {
+        prev_calced=false;
+        prev_not_paused=(int)SDL_GetTicks();
+        int time=0;
+        window.score_show(time,initial_score);
+        return initial_score;
+    }
     int time = (int)(SDL_GetTicks()/1000);
+    if(prev_calced)prev_paused=(int)(SDL_GetTicks()/1000);
     time-=extra_time+5;
     time_passed= time;
-    if(time_passed%10==0 && time_passed!=0){
+    if((time_passed%10==0 && time_passed!=0) && paused_flag==false){
         Mix_PlayChannel(-1,levelup,0);
     }
-    window.score_show(time,initial_score);
-    return initial_score+ time;
+    initial_score=time-extra+prev_score;
+    if(!prev_calced)
+    {
+        prev_not_paused=int(SDL_GetTicks()/1000);
+        //printf("%d %d ",prev_not_paused,prev_paused);
+        initial_score-=(prev_not_paused-prev_paused);
+        extra+=prev_not_paused-prev_paused;
+        //printf("%d\n",initial_score);
+        prev_calced=true;
+        time=0;
+        window.score_show(time ,initial_score);
+        return initial_score+time;
+
+    }
+    time=0;
+    window.score_show(time ,initial_score);
+    return initial_score+time;
 }
 
 
